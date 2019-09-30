5072d6dd5bcdbe940e96bc0ad6971c9e3cc13ac9
Sophie Rand <s.rand525@gmail.com>
2019-09-30 12:31:54 -0400

    added unit test

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ This repo provides `publications.json`, metadata on publications - for our publi
 
 The dataset linkage (represented by `related_dataset` field - see more below) originates from manually-curated relationships documented in multiple [drops](https://github.com/NYU-CI/RichContextMetadata/tree/master/metadata) provided by a community of researchers.
 
-## Instructions for Rich Context Janitors
+# Instructions for Adding new publications
+1. Add publications
+2. Run unit test (`test.py`)
+
+## 1. Adding publications 
 * Create branch - name it like the subdirectory of `RichContextMetadata/metadata` that you were working within.
 * Make a copy of `publications_export_template.ipynb` into the subdirectory `/publishing_partitions` and rename the notebook.
 * Step through the notebook, adjusting variable names as needed, to export your publications metdata to `/partitions`.
@@ -17,10 +21,16 @@ At a minimum, each record in the `publications.json` file must have
   * `url` OR 'doi' -- link to publication, preferably open-access if exists. Must have a `doi` if no `url`
 
 ### Additional fields
+* `date` - publication date
 * `alt_ids` -- stored as a list, other unique identifiers (alternative DOIs, ids from publishers, etc.). alt_ids should be written as a `URN` e.g, if you have a pmid 28818487 from Pubmed, the entry would be  `alt_ids`: [`'pubmed:28818487'`]
-* `keywords or categories (e.g. mesh terms)`
-* `journal/publisher`
+* `keywords` - keywords or categories (e.g. mesh terms)
+* `journal`
+* `publisher`
 * `open access`
-* `authors`  (including affiliations)
+* `authors` - authors, including affiliations if available
 * `funder`
 * `# of citations`
+
+
+## 2. Run unit test
+Run `python test.py` - a unit test on the last edited file within `partitions/`. If there are any errors,  make changes in the csv and rerun your notebook to re-export the .json file.
---
+++ test.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from urllib.parse import urlparse
+import json
+import os
+import sys
+import unittest
+import glob
+
+
+def url_validator (url):
+    """validate the format of a URL"""
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc, result.path])
+    except:
+        return False
+
+
+class TestVerifypublications (unittest.TestCase):
+    ALLOWED_FIELDS = set([
+            "alt_ids",
+            "keywords",
+            "date",
+            "doi",
+            "title",
+            "url",
+            "journal",
+            "publisher",
+            "related_dataset"
+            ])
+
+
+    def setUp (self):
+        """load the publications list"""
+        self.publications = []
+        list_of_files = glob.glob('/Users/sophierand/RCPublications/partitions/*.json') # * means all if need specific format then *.csv
+        filename = max(list_of_files, key=os.path.getctime)
+        # filename = 'foo.json'
+        self.filename = filename
+        with open(filename, "r") as f:
+            self.publications = json.load(f)
+
+
+    def test_file_loaded (self):
+        print("\n{} publications loaded".format(len(self.publications)))
+        self.assertTrue(len(self.publications) > 0)
+
+
+    def test_has_required_fields (self):
+        for publication in self.publications:
+            if not set(["title", "related_dataset"]).issubset(publication.keys()):
+                raise Exception("{}: missing required fields".format(publication["id"]))
+
+
+    def test_has_valid_url (self):
+        for publication in self.publications:
+            if "url" in publication:
+                url = publication["url"]
+
+                if url == "" or not url:
+                    pass
+                elif not url_validator(url):
+                    raise Exception("{}: badly formed URL {}".format(publication["title"], url))
+
+
+    def test_each_field (self):
+        for publication in self.publications:
+            for key in publication.keys():
+                if key not in self.ALLOWED_FIELDS:
+                    raise Exception("{}: unknown field name {}".format(publication["title"], key))
+
+
+    def test_unique_titles (self):
+        print('testing {} file now'.format(self.filename))
+        title_set = set([])
+
+        for publication in self.publications:
+            title = publication["title"]
+
+            if title in title_set:
+                raise Exception("{}: duplicate title {}".format(publication["title"], title))
+            else:
+                title_set.add(title)
+
+
+if __name__ == "__main__":
+    unittest.main()
